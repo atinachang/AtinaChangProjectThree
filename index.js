0816ea389bfbe50bcd2eb1f231418ede68a1fe85
atinachang <atinachang@gmail.com>
2020-06-02 15:56:28 -0400

    hover, active styling

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-function PlaySound(fileName) {
-    var path = "./" + fileName + ".mp3"
-    var sound = new Audio(path);
-    sound.play();
-}
+// function PlaySound(fileName) {
+//     var path = "./" + fileName + ".mp3"
+//     var sound = new Audio(path);
+//     sound.play();
+// }
+$(function() {
+	var audTag = document.getElementsByTagName('audio');
+    console.log(audTag);
+    
+    $(".southside").on("click", function() {
+        var audio = $(".p1")[0];
+        audio.play();
+    });
+    $(".bangladesh").on("click", function() {
+        var audio = $(".p2")[0];
+        audio.play();
+    });
+    $(".charlieHeat").on("click", function() {
+        var audio = $(".p3")[0];
+        audio.play();
+    });
+    $(".murdaBeatz").on("click", function() {
+        var audio = $(".p4")[0];
+        audio.play();
+    });
+    $(".daytrip").on("click", function() {
+        var audio = $(".p5")[0];
+        audio.play();
+    });
+    $(".drummaBoy").on("click", function() {
+        var audio = $(".p6")[0];
+        audio.play();
+    });
+    $(".mikeWill").on("click", function() {
+        var audio = $(".p7")[0];
+        audio.play();
+    });
+    $(".djMustard").on("click", function() {
+        var audio = $(".p8")[0];
+        audio.play();
+    });
+    $(".metroBoomin").on("click", function() {
+        var audio = $(".p9")[0];
+        audio.play();
+    });
+    $(".cannon").on("click", function() {
+        var audio = $(".p10")[0];
+        audio.play();
+    });
+    $(".blaze").on("click", function() {
+        var audio = $(".p11")[0];
+        audio.play();
+    });
+    $(".banner").on("click", function() {
+        var audio = $(".p12")[0];
+        audio.play();
+    });
+});
