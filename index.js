3c0f505360e3c665c19df869449dd5100d35270d
atinachang <atinachang@gmail.com>
2020-06-02 20:41:13 -0400

    added direction text

--- index.js
+++ index.js
@@ -1,66 +1,65 @@
 
+
 $(function() {
     var audTag = document.getElementsByTagName('audio');
-    // console.log(audTag);
-    
-    // $(".southside").on("click", "keydown", function() {
-    //     if (keyCode===87) 
-    //     var audio = $(".p1")[0];
-    //     audio.play();
-    // });
-    
-    // document.addEventListener('keydown', function(e) {
-    //     if (e.keyCode == 87) {
-    //     document.getElementsByTagName('.p1').play(audio);
-    //     }
-    // });
+        // console.log(audTag);
+
+
     $(".southside").on("click", function() {
-        var audio = $(".p1")[0];
+        const audio = $(".p1")[0];
         audio.play();
     });
+    $(document).keydown(function(e){
+        if (e.keyCode === 87) {
+            document.getElementsByClassName('p1')[0].play();
+            return false
+        }
+    })
+    
+
 
     $(".bangladesh").on("click", function() {
-        var audio = $(".p2")[0];
+        const audio = $(".p2")[0];
         audio.play();
     });
     $(".charlieHeat").on("click", function() {
-        var audio = $(".p3")[0];
+        const audio = $(".p3")[0];
         audio.play();
     });
     $(".murdaBeatz").on("click", function() {
-        var audio = $(".p4")[0];
+        const audio = $(".p4")[0];
         audio.play();
     });
     $(".daytrip").on("click", function() {
-        var audio = $(".p5")[0];
+        const audio = $(".p5")[0];
         audio.play();
     });
     $(".drummaBoy").on("click", function() {
-        var audio = $(".p6")[0];
+        const audio = $(".p6")[0];
         audio.play();
     });
     $(".mikeWill").on("click", function() {
-        var audio = $(".p7")[0];
+        const audio = $(".p7")[0];
         audio.play();
     });
     $(".djMustard").on("click", function() {
-        var audio = $(".p8")[0];
+        const audio = $(".p8")[0];
         audio.play();
     });
     $(".metroBoomin").on("click", function() {
-        var audio = $(".p9")[0];
+        const audio = $(".p9")[0];
         audio.play();
     });
     $(".cannon").on("click", function() {
-        var audio = $(".p10")[0];
+        const audio = $(".p10")[0];
         audio.play();
     });
     $(".blaze").on("click", function() {
-        var audio = $(".p11")[0];
+        const audio = $(".p11")[0];
         audio.play();
     });
     $(".banner").on("click", function() {
-        var audio = $(".p12")[0];
+        const audio = $(".p12")[0];
         audio.play();
     });
 });
