3eb77ada5f81b29cf3dfed49f500cdeec818f488
atinachang <atinachang@gmail.com>
2020-06-08 18:45:40 -0400

    adding more image lines in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-//array of objects representing each producer, name and audio that will be displayed on the page
+//array of objects representing each producer, name, photo and audio that will be displayed on the page
 const content = [
     {
         name: `southside`,
@@ -10,121 +10,141 @@ const content = [
     {
         name: "bangladesh",
         producer: `Bangladesh`,
-        audio: './audio/bangladesh.mp3',
+        audio: './audio/bangladesh.mp3',        
+        image: './images/bangladesh.jpeg',
         audioClass: "p2"
     },
     {
         name: "charlieHeat",
         producer: `Charlie Heat`,
         audio: './audio/charlieHeat.mp3',
+        image: './images/charlieHeat.jpeg',
         audioClass: 'p3',
     },
     {
         name: "metroBoomin",
         producer: `Metro Boomin`,
         audio: './audio/metro.mp3',
+        image: './images/metroBoomin.jpeg',
         audioClass: "p9"
     },
     {
         name: "murdaBeatz",
         producer: `Murda Beatz`,
         audio: './audio/murda.mp3',
+        image: './images/murdaBeatz.jpeg',
         audioClass: 'p4',
     },
     {
         name: "daytrip",
         producer: `Take A Day Trip`,
         audio: './audio/daytrip.mp3',
+        image: './images/daytrip.jpeg',
         audioClass: 'p5',
     },
     {
         name: "drummaBoy",
         producer: `Drumma Boy`,
         audio: './audio/drummaBoy.mp3',
+        image: './images/drummaBoy.jpeg',
         audioClass: 'p6',
     },
     {
         name: "mikeWill",
         producer: `Mike WiLL Made It`,
         audio: './audio/mikeWill.mp3',
+        image: './images/mikeWill.jpeg',
         audioClass: 'p7',
     },
     {
         name: "djMustard",
         producer: `DJ Mustard`,
         audio: './audio/mustard.mp3',
+        image: './images/djMustard.png',
         audioClass: 'p8',
     },
     {
         name: "donCannon",
         producer: `Don Cannon`,
         audio: './audio/cannon.mp3',
+        image: './images/donCannon.jpeg',
         audioClass: 'p10',
     },
     {
         name: "justBlaze",
         producer: `Just Blaze`,
         audio: './audio/justBlaze.mp3',
+        image: './images/justBlaze.jpeg',
         audioClass: 'p11',
     },
     {
         name: "davidBanner",
         producer: `David Banner`,
         audio: './audio/davidBanner.mp3',
+        image: './images/davidBanner.png',
         audioClass: 'p12',
     },
     {
         name: "tayKeith",
         producer: `Tay Keith`,
         audio: './audio/tayKeith.mp3',
+        image: './images/tayKeith.jpeg',
         audioClass: 'p13',
     },
     {
         name: "sonnyDigital",
         producer: `Sonny Digital`,
         audio: './audio/sonnyDigital.mp3',
+        image: './images/sonnyDigital.jpeg',
         audioClass: 'p14',
     },
     {
         name: "youngChop",
         producer: `Young Chop`,
         audio: './audio/youngChop.mp3',
+        image: './images/youngChop.jpeg',
         audioClass: 'p15',
     },
     {
         name: "harryFraud",
         producer: `Harry Fraud`,
         audio: './audio/harryFraud.mp3',
+        image: './images/harryFraud.jpeg',
         audioClass: 'p16',
     },
     {
         name: "bobbyJohnson",
         producer: `Bobby Johnson`,
         audio: './audio/bobbyJohnson.mp3',
+        image: './images/bobbyJohnson.jpeg',
         audioClass: 'p17',
     },
     {
         name: "darkChild",
         producer: `Dark Child`,
         audio: './audio/darkChild.mp3',
+        image: './images/darkChild.jpeg',
         audioClass: 'p18',
     },
     {
         name: "jahlilBeats",
         producer: `Jahlil Beats`,
         audio: './audio/jahlilBeats.mp3',
+        image: './images/jahlilBeats.jpg',
         audioClass: 'p19',
     },
     {
         name: "londonOnDaTrack",
         producer: `London On Da Track`,
         audio: './audio/londonOnDaTrack.mp3',
+        image: './images/londonOnDaTrack.jpeg',
         audioClass: 'p20',
     },
     {
         name: "maalyRaw",
         producer: `Maaly Raw`,
         audio: './audio/maalyRaw.mp3',
+        image: './images/maalyRaw.jpeg',
         audioClass: 'p21',
     },
     {
@@ -208,8 +228,8 @@ content.init = () => {
     content.forEach(block => {
         $('.container').append(`
         <li class="gridItem ${block.name}" id="${block.name}" tabindex="0">
-        <img src="${block.image}" alt="${block.producer}" class="sr-only">
-        <h2>${block.producer}</h2>  
+            <img src="${block.image}" alt="${block.producer}" class="sr-only"/>
+            <h2>${block.producer}</h2>  
         </li>
         <audio src="${block.audio}" class="${block.audioClass}" preload="auto" type="audio/mpeg"></audio>
 
